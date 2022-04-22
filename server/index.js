068bfc142146747ed7bf45f1c941816ec16358fe
MarkAlperin <mark.t.alperin@gmail.com>
2022-04-22 10:45:35 -0500

    implement get reviews functionality

--- server/index.js
+++ server/index.js
@@ -26,29 +26,48 @@ app.get("/postman", (req, res) => {
 });
 
 
-app.get('/reviews', (req, res) => {
-  Reviews.find({product_id: 37311}).exec()
+// app.get('/reviews', (req, res) => {
+//   Reviews.find({product_id: 37311}).exec()
+//     .then(data => {
+//       console.log(data);
+//       res.send(data);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//     });
+// });
+app.get("/reviews", (req, res) => {
+
+
+  const options = {
+    product_id: Number(req.query.product_id) || 1,
+    page: Number(req.query.page) || 1,
+    count: Number(req.query.count) || 5,
+    sort: null
+  };
+
+  if (req.query.sort === 'helpful') {
+    options.sort = {'helpfulness': 1};
+  } else if (req.query.sort === 'newest') {
+    options.sort = {"date": -1};
+  } else if (req.query.sort === "relevant") {
+    // TODO: how sort by relevance?
+  }
+  // console.log(options)
+  Reviews.find({"product_id": options.product_id}).skip(options.count * options.page - options.count).limit(options.count).sort(options.sort)
     .then(data => {
       console.log(data);
       res.send(data);
     })
     .catch(err => {
       console.error(err);
-    });
+      res.send(err)
+    })
 });
-// app.get("/reviews", (req, res) => {
-//   console.log('getReviews');
-//   db.getAll()
-//     .then(data => {
-//       console.log(data);
-//     })
-//     .catch(err => {
-//       console.error(err)
-//     })
-// });
 
 app.post("/postman", (req, res) => {
   console.log(req.body);
+
   res.sendStatus(200);
 });
 
