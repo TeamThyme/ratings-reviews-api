8694a8932d77e7de3ec8bbd53b5aeed46bda08e4
MarkAlperin <mark.t.alperin@gmail.com>
2022-04-28 11:42:33 -0500

    mongoose address change

--- db/index.js
+++ db/index.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 
-mongoose.connect("mongodb://markalperin:password@localhost:27017/reviews?authMechanism=SCRAM-SHA-256")
+mongoose
+  .connect("mongodb://markalperin:password@localhost:27017/?authMechanism=SCRAM-SHA-256", { dbName: 'reviews' })
+  // .connect("mongodb://markalperin:password@localhost:27017/reviews?authMechanism=SCRAM-SHA-256")
   // .connect("mongodb://localhost/reviews")
   .then((res) => console.log("You are connected to the Reviews DB!"))
   .catch((err) => console.log(err));
